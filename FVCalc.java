c26ddde91bad34adc58484395f8f6e4e36a6668e
Adi Levita <adi.levitan@post.runi.ac.il>
2025-11-05 20:23:50 +0200

    completed assignment

--- FVCalc.java
+++ FVCalc.java
@@ -1,6 +1,18 @@
 // Computes the future value of a saving investment.
 public class FVCalc {
 	public static void main(String[] args){
-		// Replace this comment with your code
+	
+		// הסכום הנוכחי + ריבית שנתית + מספר שנים
+		int currentValue = Integer.parseInt (args[0]);
+		double rate = Double.parseDouble(args[1])/ 100;
+		int years = Integer.parseInt(args[2]);
+
+		//מציאת הערך
+		double futureValue = currentValue * Math.pow(1 + rate, years);
+
+		// הדפסה 
+		System.out.println("After " + years + " years, $" + currentValue + " saved at " + (rate * 100) + "% will yield " + (int)futureValue);
+		
+		// end
 	}
 }
